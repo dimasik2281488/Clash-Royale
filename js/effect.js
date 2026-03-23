c6a60d08d376ac6d288156becafa20f2d261ba56
dimasik2281488 <dima93252@gmail.com>
2026-03-23 16:47:54 +0300

    Update effect.js

--- js/effect.js
+++ js/effect.js
@@ -10,3 +10,22 @@ window.drawSpawnEffect = function(ctx, x, y) {
         ctx.stroke();
     }
 }
+
+
+// Функция для рисования молнии
+window.drawLightning = function(ctx, x1, y1, x2, y2) {
+    // TODO: Ломаная линия-зигзаг
+    
+    ctx.strokeStyle = '#ffff00';
+    ctx.lineWidth = 4;
+    
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    
+    const midX = (x1 + x2) / 2 + (Math.random() * 20 - 10);
+    const midY = (y1 + y2) / 2 + (Math.random() * 20 - 10);
+    
+    ctx.lineTo(midX, midY);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
